3a65e053b77c2fe72f7aa59dc1e69b93b2e2f569
Keiji Yoshimi <walf443@gmail.com>
2022-05-14 19:00:51 +0900

    added publish_topic sample

--- pubsub-sample/src/main.rs
+++ pubsub-sample/src/main.rs
@@ -1,7 +1,14 @@
 use google_api_proto::google::pubsub::v1::publisher_client::PublisherClient;
-use google_api_proto::google::pubsub::v1::{ListTopicsRequest, Topic};
+use google_api_proto::google::pubsub::v1::{
+    ListTopicsRequest, PublishRequest, PubsubMessage, Topic,
+};
+use tonic::codegen::Bytes;
 use tonic::transport::Channel;
 use tonic::{Code, Request};
+
+type PubSub = PublisherClient<Channel>;
+
+struct TopicName(String);
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -10,19 +17,22 @@ async fn main() -> anyhow::Result<()> {
         .await?;
 
     let mut pubsub = PublisherClient::new(channel);
-    create_topic(&mut pubsub).await?;
+    let topic_name = TopicName("projects/test/topics/test".to_string());
+    create_topic(&mut pubsub, &topic_name).await?;
 
     list_topic(&mut pubsub).await?;
+
+    publish_topic(&mut pubsub, &topic_name).await?;
 
     Ok(())
 }
 
-async fn create_topic(pubsub: &mut PublisherClient<Channel>) -> anyhow::Result<()> {
+async fn create_topic(pubsub: &mut PubSub, topic_name: &TopicName) -> anyhow::Result<()> {
     println!("create topic");
 
     let res = pubsub
         .create_topic(Request::new(Topic {
-            name: "projects/test/topics/test".to_string(),
+            name: topic_name.0.clone(),
             labels: Default::default(),
             message_storage_policy: None,
             kms_key_name: "".to_string(),
@@ -43,7 +53,7 @@ async fn create_topic(pubsub: &mut PublisherClient<Channel>) -> anyhow::Result<(
     Ok(())
 }
 
-async fn list_topic(pubsub: &mut PublisherClient<Channel>) -> anyhow::Result<()> {
+async fn list_topic(pubsub: &mut PubSub) -> anyhow::Result<()> {
     println!("list topics");
 
     let res = pubsub
@@ -57,6 +67,27 @@ async fn list_topic(pubsub: &mut PublisherClient<Channel>) -> anyhow::Result<()>
     for topic in &res.get_ref().topics {
         println!("topic = {:#?}", topic);
     }
+
+    Ok(())
+}
+
+async fn publish_topic(pubsub: &mut PubSub, topic_name: &TopicName) -> anyhow::Result<()> {
+    println!("publish topic");
+
+    let res = pubsub
+        .publish(PublishRequest {
+            topic: topic_name.0.clone(),
+            messages: vec![PubsubMessage {
+                data: Bytes::from_static(b"this is test"),
+                attributes: Default::default(),
+                message_id: "1".to_string(),
+                publish_time: None,
+                ordering_key: "1".to_string(),
+            }],
+        })
+        .await?;
+
+    println!("response = {:#?}", res);
 
     Ok(())
 }
