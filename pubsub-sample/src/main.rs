29067968d852bda3d7884b9feaafc5225138a78a
Keiji Yoshimi <walf443@gmail.com>
2022-05-14 18:40:03 +0900

    refactor logic

--- pubsub-sample/src/main.rs
+++ pubsub-sample/src/main.rs
@@ -1,47 +1,62 @@
-use google_api_proto::google::pubsub::v1::{ListTopicsRequest, PublishRequest, Topic};
 use google_api_proto::google::pubsub::v1::publisher_client::PublisherClient;
-use tonic::{Code, Request, Status};
+use google_api_proto::google::pubsub::v1::{ListTopicsRequest, Topic};
 use tonic::transport::Channel;
+use tonic::{Code, Request};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("create topic");
-    let channel = Channel::from_static("http://localhost:8085").connect().await?;
+    let channel = Channel::from_static("http://localhost:8085")
+        .connect()
+        .await?;
 
     let mut pubsub = PublisherClient::new(channel);
-    let res = pubsub.create_topic(Request::new(Topic {
-        name: "projects/test/topics/test".to_string(),
-        labels: Default::default(),
-        message_storage_policy: None,
-        kms_key_name: "".to_string(),
-        schema_settings: None,
-        satisfies_pzs: false,
-        message_retention_duration: None
-    })).await;
+    create_topic(&mut pubsub).await?;
 
-    if res.is_err() {
+    list_topic(&mut pubsub).await?;
 
-    }
+    Ok(())
+}
+
+async fn create_topic(pubsub: &mut PublisherClient<Channel>) -> anyhow::Result<()> {
+    println!("create topic");
+
+    let res = pubsub
+        .create_topic(Request::new(Topic {
+            name: "projects/test/topics/test".to_string(),
+            labels: Default::default(),
+            message_storage_policy: None,
+            kms_key_name: "".to_string(),
+            schema_settings: None,
+            satisfies_pzs: false,
+            message_retention_duration: None,
+        }))
+        .await;
+
     if let Err(status) = res {
-        if status.code() !=  Code::AlreadyExists {
+        if status.code() != Code::AlreadyExists {
             return Err(status.into());
         }
     } else {
         println!("response = {:#?}", res);
     }
 
+    Ok(())
+}
+
+async fn list_topic(pubsub: &mut PublisherClient<Channel>) -> anyhow::Result<()> {
     println!("list topics");
 
-    let res = pubsub.list_topics(Request::new(ListTopicsRequest {
-        project: format!("projects/{}", "test"),
-        page_size: 10,
-        ..Default::default()
-    })).await?;
+    let res = pubsub
+        .list_topics(Request::new(ListTopicsRequest {
+            project: format!("projects/{}", "test"),
+            page_size: 10,
+            ..Default::default()
+        }))
+        .await?;
 
     for topic in &res.get_ref().topics {
         println!("topic = {:#?}", topic);
     }
-
 
     Ok(())
 }
